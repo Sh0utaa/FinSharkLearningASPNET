f158aa400102adc3f459d0fb30f9405a216bd29b
Sh0utaa <stevdorashvili08@gmail.com>
2024-08-24 11:23:46 +0200

    Identity Framework

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
         public ApplicationDBContext(DbContextOptions dbContextOptions)
         : base(dbContextOptions)
@@ -18,5 +19,44 @@ namespace api.Data
         public DbSet<Stock> Stocks { get; set; }
 
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seeding Stock data
+            modelBuilder.Entity<Stock>().HasData(
+                new Stock
+                {
+                    Id = 1,
+                    Symbol = "AAPL",
+                    CompanyName = "Apple Inc.",
+                    Purchase = 145.00M,
+                    LastDiv = 0.82M,
+                    Industry = "Technology",
+                    MarketCap = 2230000000000 // in billions
+                },
+                new Stock
+                {
+                    Id = 2,
+                    Symbol = "MSFT",
+                    CompanyName = "Microsoft Corp.",
+                    Purchase = 299.00M,
+                    LastDiv = 0.62M,
+                    Industry = "Technology",
+                    MarketCap = 2120000000000 // in billions
+                },
+                new Stock
+                {
+                    Id = 3,
+                    Symbol = "NVDA",
+                    CompanyName = "NVIDIA Corp.",
+                    Purchase = 500.00M,
+                    LastDiv = 0.40M,
+                    Industry = "Semiconductors",
+                    MarketCap = 1090000000000 // in billions
+                }
+            );
+        }
     }
 }
---
+++ api/Migrations/20240823155452_SeedInitialStocks.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialStocks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Stocks",
+                columns: new[] { "Id", "CompanyName", "Industry", "LastDiv", "MarketCap", "Purchase", "Symbol" },
+                values: new object[,]
+                {
+                    { 1, "Apple Inc.", "Technology", 0.82m, 2230000000000L, 145.00m, "AAPL" },
+                    { 2, "Microsoft Corp.", "Technology", 0.62m, 2120000000000L, 299.00m, "MSFT" },
+                    { 3, "NVIDIA Corp.", "Semiconductors", 0.40m, 1090000000000L, 500.00m, "NVDA" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
